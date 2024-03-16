b870d9f9c894fd31e07b7bbd10e272998beade01
javeriishika <126329582+javeriishika@users.noreply.github.com>
2024-03-16 05:25:19 +0000

    Flask Codespace

---
+++ test_app.py
@@ -0,0 +1,22 @@
+import app
+import json
+import warnings
+
+def test_name():
+    response = app.app.test_client().get('/hello')
+    assert response.status_code == 200
+    assert response.data == b'hello'
+
+def test_name():
+    response = app.app.test_client().get('/hello')
+    assert response.status_code == 200
+    assert response.data == b'hello'
+
+def test_goodbye():
+    response = app.app.test_client().get("/goodbye")
+    assert response.status_code == 404
+
+def test_name():
+     response = app.app.test_client().get("/name")
+     assert response.status_code == 200
+     assert response.data == b'Ishika'
